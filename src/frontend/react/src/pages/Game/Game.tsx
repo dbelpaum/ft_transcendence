3716f7d5fc75c5433f5b6840511a9893124063ea
dbelpaum <douglefoudoug@yahoo.fr>
2023-11-26 21:30:01 +0100

    fix: fluidité gameplay + angle en fct du hitIndex + debut camera 3D

--- src/frontend/react/src/pages/Game/Game.tsx
+++ src/frontend/react/src/pages/Game/Game.tsx
@@ -6,25 +6,24 @@ import GameModeButtons from '../../components/GameModeButtons/GameModeButtons';
 
 const Game: React.FC = () => {
 	const [selectedGameMode, setSelectedGameMode] = useState<string>(''); // Initial state
-	const [playerPaddlePosition, setPlayerPaddlePosition] = useState<number>(0);
+
   
 	const handleSelectMode = (mode: string) => {
 	  setSelectedGameMode(mode);
 	};
 
-	const handlePlayerMove = (deltaX: number) => {
-		// Update the player paddle position or perform other actions as needed
-		setPlayerPaddlePosition((prevPosition) => prevPosition + deltaX);
-	};
+
   
 	return (
 		<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh' }}>
 		  {!selectedGameMode && <GameModeButtons onSelectMode={handleSelectMode} />}
+		  <div id="gameScore"></div>
 		  <GameScene
 			width={800}
 			height={600}
 			initialGameMode={selectedGameMode}
 		  />
+		  <div id="info"></div>
 		</div>
 	  );
 	};
