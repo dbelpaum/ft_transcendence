3258d6e5d16ec7afd1b0bceeadf768f31004beae
Sam Acer <sam@sam.fr>
2023-11-27 18:02:30 +0100

    feat:Nest a maintenant les bonnes dependances pour l'authentification, et l'authentification est demande si on va sur authentification/42

--- src/backend/nest/dist/main.js
+++ src/backend/nest/dist/main.js
@@ -66,12 +66,13 @@ const app_controller_1 = __webpack_require__(/*! ./app.controller */ "./src/app.
 const app_service_1 = __webpack_require__(/*! ./app.service */ "./src/app.service.ts");
 const sam_test_controller_1 = __webpack_require__(/*! ./sam-test/sam-test.controller */ "./src/sam-test/sam-test.controller.ts");
 const prisma_service_1 = __webpack_require__(/*! ./prisma.service */ "./src/prisma.service.ts");
+const authentification_module_1 = __webpack_require__(/*! ./authentification/authentification.module */ "./src/authentification/authentification.module.ts");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
-        imports: [],
+        imports: [authentification_module_1.AuthentificationModule],
         controllers: [app_controller_1.AppController, sam_test_controller_1.SamTestController],
         providers: [app_service_1.AppService, prisma_service_1.PrismaService],
     })
@@ -105,6 +106,137 @@ exports.AppService = AppService;
 exports.AppService = AppService = __decorate([
     (0, common_1.Injectable)()
 ], AppService);
+
+
+/***/ }),
+
+/***/ "./src/authentification/authentification.controller.ts":
+/*!*************************************************************!*\
+  !*** ./src/authentification/authentification.controller.ts ***!
+  \*************************************************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.AuthentificationController = void 0;
+const common_1 = __webpack_require__(/*! @nestjs/common */ "@nestjs/common");
+const passport_1 = __webpack_require__(/*! @nestjs/passport */ "@nestjs/passport");
+let AuthentificationController = class AuthentificationController {
+    async login42(req) {
+    }
+    async callback42(req) {
+        return req.user;
+    }
+};
+exports.AuthentificationController = AuthentificationController;
+__decorate([
+    (0, common_1.Get)('42'),
+    (0, common_1.UseGuards)((0, passport_1.AuthGuard)('42')),
+    __param(0, (0, common_1.Req)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", Promise)
+], AuthentificationController.prototype, "login42", null);
+__decorate([
+    (0, common_1.Get)('42/callback'),
+    (0, common_1.UseGuards)((0, passport_1.AuthGuard)('42')),
+    __param(0, (0, common_1.Req)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", Promise)
+], AuthentificationController.prototype, "callback42", null);
+exports.AuthentificationController = AuthentificationController = __decorate([
+    (0, common_1.Controller)('authentification')
+], AuthentificationController);
+
+
+/***/ }),
+
+/***/ "./src/authentification/authentification.module.ts":
+/*!*********************************************************!*\
+  !*** ./src/authentification/authentification.module.ts ***!
+  \*********************************************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.AuthentificationModule = void 0;
+const common_1 = __webpack_require__(/*! @nestjs/common */ "@nestjs/common");
+const authentification_controller_1 = __webpack_require__(/*! ./authentification.controller */ "./src/authentification/authentification.controller.ts");
+const fortytwo_service_1 = __webpack_require__(/*! ./fortytwo/fortytwo.service */ "./src/authentification/fortytwo/fortytwo.service.ts");
+let AuthentificationModule = class AuthentificationModule {
+};
+exports.AuthentificationModule = AuthentificationModule;
+exports.AuthentificationModule = AuthentificationModule = __decorate([
+    (0, common_1.Module)({
+        controllers: [authentification_controller_1.AuthentificationController],
+        providers: [fortytwo_service_1.FortyTwoService]
+    })
+], AuthentificationModule);
+
+
+/***/ }),
+
+/***/ "./src/authentification/fortytwo/fortytwo.service.ts":
+/*!***********************************************************!*\
+  !*** ./src/authentification/fortytwo/fortytwo.service.ts ***!
+  \***********************************************************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.FortyTwoService = void 0;
+const common_1 = __webpack_require__(/*! @nestjs/common */ "@nestjs/common");
+const passport_1 = __webpack_require__(/*! @nestjs/passport */ "@nestjs/passport");
+const passport_42_1 = __webpack_require__(/*! passport-42 */ "passport-42");
+let FortyTwoService = class FortyTwoService extends (0, passport_1.PassportStrategy)(passport_42_1.Strategy, '42') {
+    constructor() {
+        super({
+            clientID: process.env.FORTYTWO_APP_ID,
+            clientSecret: process.env.FORTYTWO_APP_SECRET,
+            callbackURL: process.env.FORTYTWO_CALLBACK,
+        });
+    }
+    async validate(accessToken, refreshToken, profile) {
+        const user = {
+            fortytwoId: profile.id,
+            accessToken
+        };
+        return user;
+    }
+};
+exports.FortyTwoService = FortyTwoService;
+exports.FortyTwoService = FortyTwoService = __decorate([
+    (0, common_1.Injectable)(),
+    __metadata("design:paramtypes", [])
+], FortyTwoService);
 
 
 /***/ }),
@@ -217,6 +349,16 @@ module.exports = require("@nestjs/core");
 
 /***/ }),
 
+/***/ "@nestjs/passport":
+/*!***********************************!*\
+  !*** external "@nestjs/passport" ***!
+  \***********************************/
+/***/ ((module) => {
+
+module.exports = require("@nestjs/passport");
+
+/***/ }),
+
 /***/ "@prisma/client":
 /*!*********************************!*\
   !*** external "@prisma/client" ***!
@@ -224,6 +366,26 @@ module.exports = require("@nestjs/core");
 /***/ ((module) => {
 
 module.exports = require("@prisma/client");
+
+/***/ }),
+
+/***/ "dotenv":
+/*!*************************!*\
+  !*** external "dotenv" ***!
+  \*************************/
+/***/ ((module) => {
+
+module.exports = require("dotenv");
+
+/***/ }),
+
+/***/ "passport-42":
+/*!******************************!*\
+  !*** external "passport-42" ***!
+  \******************************/
+/***/ ((module) => {
+
+module.exports = require("passport-42");
 
 /***/ })
 
@@ -265,7 +427,9 @@ var exports = __webpack_exports__;
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core_1 = __webpack_require__(/*! @nestjs/core */ "@nestjs/core");
 const app_module_1 = __webpack_require__(/*! ./app.module */ "./src/app.module.ts");
+const dotenv = __webpack_require__(/*! dotenv */ "dotenv");
 async function bootstrap() {
+    dotenv.config();
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
     await app.listen(4000);
