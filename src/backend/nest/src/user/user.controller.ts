8c6940cef44b97feffdf01acc4007e4039696980
Yamisan0 <idris.messaad@gmail.com>
2023-12-13 23:14:48 +0100

    ajout de champs dans la bdd, on stock maintenant les infos du user lors de la premiere connexion

--- src/backend/nest/src/user/user.controller.ts
+++ src/backend/nest/src/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private prismaService: PrismaService) {}
+
+    @Post('create')
+    async create(@Body() body: {username: string; email: string}) {
+        return this.prismaService.user.create({
+            data: {
+                username: body.username,
+                email: body.email,
+            }
+        })
+    }
+
+}
