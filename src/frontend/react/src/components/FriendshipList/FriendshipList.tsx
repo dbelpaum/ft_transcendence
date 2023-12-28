cf5ecec2ef3e03d4661ad336bc284ce9b0c9e3ca
Yamisan0 <idris.messaad@gmail.com>
2023-12-28 13:35:11 +0100

    composant fonctionne correctement reste plus qu'a securiser le endpoint et rendre le pseudo cliquable dans la liste

--- src/frontend/react/src/components/FriendshipList/FriendshipList.tsx
+++ src/frontend/react/src/components/FriendshipList/FriendshipList.tsx
@@ -1,16 +1,33 @@
 import React, { useState , useEffect} from 'react';
 import './FriendshipList.css';
 
-type User = {
-  id: number;
-  name: string;
-};
 
-const FriendshipList: React.FC<{ userId: number }> = ({ userId }) => {
-    const [showBlockedUsers, setShowBlockedUsers] = useState(false);
-    const [showFriends, setShowFriends] = useState(false);
-    const [blockedUsers, setBlockedUsers] = useState<User[]>([]);
-    const [friends, setFriends] = useState<User[]>([]);
+interface FriendshipListProps {
+  userId: number;
+}
+
+export type User = {
+    id: number;
+    pseudo: string;
+    firstname?: string;
+    lastname?: string;
+    imageURL?: string;
+  };
+  
+  export type Friendship = {
+    id: number;
+    status: 'friend' | 'blocked';
+    requester: User;
+    addressee: User;
+  };
+
+
+
+const FriendshipList: React.FC<FriendshipListProps> = ({ userId }) => {
+  const [friends, setFriends] = useState<Friendship[]>([]);
+  const [blockedUsers, setBlockedUsers] = useState<Friendship[]>([]);
+  const [showFriends, setShowFriends] = useState(false);
+const [showBlockedUsers, setShowBlockedUsers] = useState(false);
 
   useEffect(() => {
     fetch(`http://localhost:4000/friendship/${userId}/friends-and-blocked`)
@@ -22,33 +39,37 @@ const FriendshipList: React.FC<{ userId: number }> = ({ userId }) => {
       .catch(error => console.error('Error fetching data:', error));
   }, [userId]);
 
-
-
   return (
-    <div className="friendshipList-container">
-      <div className="list-option" onClick={() => setShowBlockedUsers(!showBlockedUsers)}>
-        Blocked Users {showBlockedUsers ? '▲' : '▼'}
-      </div>
-      {showBlockedUsers && (
-        <ul>
-          {blockedUsers.map((user) => (
-            <li key={user.id}>{user.name}</li>
-          ))}
-        </ul>
-      )}
+    <div>
+  <div className="list-option" onClick={() => setShowFriends(!showFriends)}>
+    Friends {showFriends ? '▲' : '▼'}
+  </div>
+  {showFriends && (
+    <ul>
+      {friends.map(friendship => (
+        <li key={friendship.id}>
+          {friendship.addressee.pseudo}
+        </li>
+      ))}
+    </ul>
+  )}
 
-      <div className="list-option" onClick={() => setShowFriends(!showFriends)}>
-        Friends {showFriends ? '▲' : '▼'}
-      </div>
-      {showFriends && (
-        <ul>
-          {friends.map((user) => (
-            <li key={user.id}>{user.name}</li>
-          ))}
-        </ul>
-      )}
-    </div>
+  <div className="list-option" onClick={() => setShowBlockedUsers(!showBlockedUsers)}>
+    Blocked Users {showBlockedUsers ? '▲' : '▼'}
+  </div>
+  {showBlockedUsers && (
+    <ul>
+      {blockedUsers.map(blocked => (
+        <li key={blocked.id}>
+          {blocked.addressee.pseudo}
+        </li>
+      ))}
+    </ul>
+  )}
+</div>
+
   );
 };
 
 export default FriendshipList;
+
